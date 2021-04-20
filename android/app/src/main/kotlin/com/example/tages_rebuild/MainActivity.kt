fb6380e400bd019e3e1a5900866844e30c8b31c0
vsevolodmur <vsevolod.mailto@gmail.com>
2021-04-20 12:13:48 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/tages_rebuild/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tages_rebuild
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/homepage/homepage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:tages_rebuild/pages/homepage/topfirstclipper.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(99.0),
+        child: AppBar(
+          elevation: 30.0,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xff3827B4), Color(0xff6C18A4)],
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          ClipPath(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xff3827B4),
+                    Color(0xff6C18A4),
+                  ],
+                ),
+              ),
+            ),
+            clipper: TopFirstClipper(),
+          ),
+        ],
+      ),
+    );
+  }
+}
